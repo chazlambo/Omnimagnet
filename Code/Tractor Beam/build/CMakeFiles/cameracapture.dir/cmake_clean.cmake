1ad6f701b17da17ccc94673d0252f0adfdc73c44
chazlambo <charlie.lambert@gmail.com>
2025-07-22 16:25:56 -0600

    Camera working but distorted

---
+++ Code/Tractor Beam/build/CMakeFiles/cameracapture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cameracapture.dir/cameracapture.cpp.o"
+  "libcameracapture.a"
+  "libcameracapture.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cameracapture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
