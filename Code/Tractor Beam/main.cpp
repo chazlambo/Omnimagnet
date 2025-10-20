d017d24844749f57ae97869554503f916f7e8b2a
chazlambo <charlie.lambert@gmail.com>
2025-10-20 11:59:19 -0600

    Experiments E4-E6

--- Code/Tractor Beam/main.cpp
+++ Code/Tractor Beam/main.cpp
@@ -119,7 +119,7 @@ int main () {
     // Timelapse Settings
     bool save_data = true;                      // Toggle timelapse
     int timelapse_interval_ms = 2000;           // Time between photos [ms] (default 2000)
-    string experiment_name = "experiment_D6";   // Creates subfolder in /output
+    string experiment_name = "experiment_E6";   // Creates subfolder in /output
 
     // If camera enabled
     CameraCapture cam(camList.GetByIndex(0));
