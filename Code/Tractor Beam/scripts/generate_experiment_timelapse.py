845b9fd52c46eec5769676f0c5692bf3ada4e3bd
chazlambo <81743171+chazlambo@users.noreply.github.com>
2025-10-22 16:02:28 -0600

    Videos for G4-G6

--- Code/Tractor Beam/scripts/generate_experiment_timelapse.py
+++ Code/Tractor Beam/scripts/generate_experiment_timelapse.py
@@ -23,9 +23,9 @@ from scipy.optimize import linear_sum_assignment
 
 # 2 Raft Experiments
 EXPERIMENTS_TO_PROCESS = [
-    "experiment_E4",
-    "experiment_E5",
-    "experiment_E6",
+    "experiment_G4",
+    "experiment_G5",
+    "experiment_G6",
 ]
 
 
